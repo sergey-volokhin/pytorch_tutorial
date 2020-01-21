ac54fdafc25fcbe54d4dc3995791f68c1b5bc6f4
Jason Choi <boslbi92@gmail.com>
2020-01-20 19:40:46 -0500

    indent

--- 2-NCF/practice.py
+++ 2-NCF/practice.py
@@ -8,11 +8,11 @@ class simpleCF(nn.Module):
 		# TODO: user_num: total num of users
 		# TODO: item_num: total num of items
 		# TODO: output_dim: number of predictive factors (neurons) on last MLP layer
-        # TODO: initialize layers and activations here based on these dimensions
+		# TODO: initialize layers and activations here based on these dimensions
 
 	def forward(self, user, item):
 		# TODO: receive two inputs <- (batch_size, users), (batch_size, items)
-        # TODO: output -> one output tensor of predicted ratings (batch_size)
+		# TODO: output -> one output tensor of predicted ratings (batch_size)
 		# TODO: implement forward pass based on figure 2, https://arxiv.org/pdf/1708.05031.pdf
 		if torch.cuda.is_available():
 			dummy_output = torch.ones(user.shape[0], device='cuda')
