c4c13b35557310337ab76904f01a21bfdf5d9efe
Sergey <>
2020-02-15 21:27:24 -0500

    initial

--- 2-NCF/neuralCF.py
+++ 2-NCF/neuralCF.py
@@ -1,79 +1,80 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
+
 
 # full neural collaborative filtering module
 class NCF(nn.Module):
-	def __init__(self, user_num, item_num, output_dim):
-		super(NCF, self).__init__()
-		self.GMF_layers = GMF(user_num=user_num, item_num=item_num, output_dim=output_dim)
-		self.MLP_layers = MLP(user_num=user_num, item_num=item_num, output_dim=output_dim)
-		self.predict_layer = nn.Linear(output_dim * 2, 1)
-		self.dropout = nn.Dropout()
+    def __init__(self, user_num, item_num, output_dim):
+        super(NCF, self).__init__()
+        self.GMF_layers = GMF(user_num=user_num, item_num=item_num, output_dim=output_dim)
+        self.MLP_layers = MLP(user_num=user_num, item_num=item_num, output_dim=output_dim)
+        self.predict_layer = nn.Linear(output_dim * 2, 1)
+        self.dropout = nn.Dropout()
 
-	def forward(self, user, item):
-		# GMF layers
-		output_GMF = self.GMF_layers(user, item)
+    def forward(self, user, item):
+        # GMF layers
+        output_GMF = self.GMF_layers(user, item)
 
-		# MLP layers
-		output_MLP = self.MLP_layers(user, item)
+        # MLP layers
+        output_MLP = self.MLP_layers(user, item)
 
-		# merge outputs
-		concat = torch.cat((output_GMF, output_MLP), -1)
-		concat = self.dropout(concat)
-		prediction = self.predict_layer(concat).view(-1)
-		return prediction
+        # merge outputs
+        concat = torch.cat((output_GMF, output_MLP), -1)
+        concat = self.dropout(concat)
+        prediction = self.predict_layer(concat).view(-1)
+        return prediction
 
 
 # multilayer perceptron sub-module
 class MLP(nn.Module):
-	def __init__(self, user_num, item_num, output_dim):
-		super(MLP, self).__init__()
-		self.user_emb = nn.Embedding(user_num, output_dim * 4)
-		self.item_emb = nn.Embedding(item_num, output_dim * 4)
+    def __init__(self, user_num, item_num, output_dim):
+        super(MLP, self).__init__()
+        self.user_emb = nn.Embedding(user_num, output_dim * 4)
+        self.item_emb = nn.Embedding(item_num, output_dim * 4)
 
-		self.linear_1 = nn.Linear(output_dim * 8, output_dim * 4)
-		self.linear_2 = nn.Linear(output_dim * 4, output_dim * 2)
-		self.linear_3 = nn.Linear(output_dim * 2, output_dim)
-		self.dropout = nn.Dropout()
-		self.relu = nn.ReLU()
+        self.linear_1 = nn.Linear(output_dim * 8, output_dim * 4)
+        self.linear_2 = nn.Linear(output_dim * 4, output_dim * 2)
+        self.linear_3 = nn.Linear(output_dim * 2, output_dim)
+        self.dropout = nn.Dropout()
+        self.relu = nn.ReLU()
 
-	def forward(self, user, item):
-		user_emb = self.user_emb(user)
-		item_emb = self.item_emb(item)
-		concat = torch.cat((user_emb, item_emb), -1)
+    def forward(self, user, item):
+        user_emb = self.user_emb(user)
+        item_emb = self.item_emb(item)
+        concat = torch.cat((user_emb, item_emb), -1)
 
-		x = self.linear_1(concat)
-		x = self.relu(x)
-		x = self.dropout(x)
+        x = self.linear_1(concat)
+        x = self.relu(x)
+        x = self.dropout(x)
 
-		x = self.linear_2(x)
-		x = self.relu(x)
-		x = self.dropout(x)
+        x = self.linear_2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
 
-		x = self.linear_3(x)
-		x = self.relu(x)
-		return x
+        x = self.linear_3(x)
+        x = self.relu(x)
+        return x
 
-	def _init_weight_(self):
-		nn.init.normal_(self.user_emb.weight, std=0.01)
-		nn.init.normal_(self.item_emb.weight, std=0.01)
+    def _init_weight_(self):
+        nn.init.normal_(self.user_emb.weight, std=0.01)
+        nn.init.normal_(self.item_emb.weight, std=0.01)
 
 
 # generalized matrix factorization sub-module
 class GMF(nn.Module):
-	def __init__(self, user_num, item_num, output_dim):
-		super(GMF, self).__init__()
-		self.user_emb = nn.Embedding(user_num, output_dim)
-		self.item_emb = nn.Embedding(item_num, output_dim)
-		self._init_weight_()
+    def __init__(self, user_num, item_num, output_dim):
+        super(GMF, self).__init__()
+        self.user_emb = nn.Embedding(user_num, output_dim)
+        self.item_emb = nn.Embedding(item_num, output_dim)
+        self._init_weight_()
 
-	def forward(self, user, item):
-		user_emb = self.user_emb(user)
-		item_emb = self.item_emb(item)
-		output = user_emb * item_emb
-		return output
+    def forward(self, user, item):
+        user_emb = self.user_emb(user)
+        item_emb = self.item_emb(item)
+        output = user_emb * item_emb
+        return output
 
-	def _init_weight_(self):
-		nn.init.normal_(self.user_emb.weight, std=0.01)
-		nn.init.normal_(self.item_emb.weight, std=0.01)
+    def _init_weight_(self):
+        nn.init.normal_(self.user_emb.weight, std=0.01)
+        nn.init.normal_(self.item_emb.weight, std=0.01)
--- 2-NCF/train.py
+++ 2-NCF/train.py
@@ -5,93 +5,143 @@ from practice import simpleCF
 
 import numpy as np
 import pandas as pd
+from gensim import models
+
 import torch
 import torch.nn as nn
 import torch.utils.data as data
-import os, time
+import os
+
+'''
+there are >95000 actors, >4000 directors, >5000 tags.
+one-hot-encoding is not practical at this point, so we will not be using them as features.
+
+'''
 
 if torch.cuda.is_available():
-	device = 'cuda'
+    device = 'cuda'
 else:
-	device = 'cpu'
-torch.manual_seed(10)
+    device = 'cpu'
+# torch.manual_seed(10)
 print('training on device:{}'.format(device.upper()))
 
 
-# data processing function to read txt file and output pytorch dataloaders
+'''
+    i am using pre-trained Google News Word2Vec model for tags aggregating,
+    so please download it and gunzip it (https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit)
+    and specify the path to it in path_to_word2vec variable
+'''
+path_to_word2vec = '/media/thejdxfh/Windows/Users/volok/Desktop/GoogleNews-vectors-negative300.bin'
+w = models.KeyedVectors.load_word2vec_format(path_to_word2vec, binary=True)
+
+
+'''
+    for each tag get average embeddings for each word,
+    multiply by the weight of that tag,
+    and take the average of all tags
+'''
+def get_avg_embedding_for_movie(row):
+    global groupped
+
+    result = []
+    try:
+        for _, group in groupped.get_group(row['movieID']).iterrows():
+            text = np.array([w[word] for word in group['value'].split() if word in w.vocab])
+            if text.size > 0:
+                result.append(text.mean(axis=0) * group['tagWeight'])
+    except KeyError:
+        return np.zeros(300)
+    return np.mean(result, axis=0)
+
+
 def process_data(device, batch_size):
-	train_rating = os.getcwd() + '/movie_train.txt'
-	test_rating = os.getcwd() + '/movie_test.txt'
-	train_data = pd.read_csv(train_rating, sep='\t', header=None, names=['user', 'item', 'rating'], usecols=[0, 1, 2])
-	test_data = pd.read_csv(test_rating, sep='\t', header=None, names=['user', 'item', 'rating'], usecols=[0, 1, 2])
+    global groupped
 
-	# user item stats
-	all_data = pd.concat([train_data, test_data])
-	num_user = len(all_data['user'].unique()) + 1
-	num_item = len(all_data['item'].unique()) + 1
+    datapath = '../../data/'
+    hetrec = datapath + 'hetrec2011-movielens-2k-v2/'
+    user_item_matrix = pd.read_csv(datapath + 'ml-latest-small/ratings.csv', usecols=[0, 1, 2]).rename(columns={'movieId': 'movieID'})
+    movies = pd.read_csv(datapath + 'ml-latest-small/movies.csv', usecols=[0, 1])
 
-	# convert input to torch tensors
-	train_user = torch.tensor(train_data['user'].values, device=device)
-	train_item = torch.tensor(train_data['item'].values, device=device)
-	train_rating = torch.tensor(train_data['rating'].values, device=device, dtype=torch.float)
+    # get rt metadata
+    movies_meta = pd.read_csv(hetrec + 'movies.dat', sep='\t', encoding='raw_unicode_escape').rename(columns={'id': 'movieID'}).drop(['spanishTitle', 'imdbID', 'title', 'imdbPictureURL', 'rtID', 'rtAllCriticsNumReviews', 'rtTopCriticsNumReviews', 'rtPictureURL'], axis=1)
+    movies_meta = movies_meta[movies_meta['movieID'].isin(user_item_matrix['movieID'].unique())]
 
-	test_user = torch.tensor(test_data['user'].values, device=device)
-	test_item = torch.tensor(test_data['item'].values, device=device)
-	test_rating = torch.tensor(test_data['rating'].values, device=device, dtype=torch.float)
+    # get the average tags embeddings
+    movie_tags = pd.read_csv(hetrec + 'movie_tags.dat', sep='\t')
+    tags = pd.read_csv(hetrec + 'tags.dat', sep='\t', encoding='raw_unicode_escape').rename(columns={'id': 'tagID'})
+    movie_tags = pd.merge(movie_tags, tags, on='tagID').sort_values(by=['movieID', 'tagID']).drop(['tagID'], axis=1)
+    groupped = movie_tags.groupby('movieID')
+    embedded_tags = pd.DataFrame.from_records(movies_meta.apply(get_avg_embedding_for_movie, axis=1), columns=[f'w2v_{i}' for i in range(1, 301)])
+    movies_meta = pd.concat([movies_meta, embedded_tags], axis=1)
 
-	# convert tensors to dataloader
-	train_dataset = data.TensorDataset(train_user, train_item, train_rating)
-	train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-	test_dataset = data.TensorDataset(test_user, test_item, test_rating)
-	test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-	print ('train_samples:{} \t test_samples:{} \t num_user:{} \t num_item:{}'.format(len(train_rating), len(test_rating), num_user, num_item))
-	return train_loader, test_loader, num_user, num_item
+    user_item_matrix = pd.merge(user_item_matrix, movies_meta, on='movieID').sort_values(by=['userId', 'movieID'])
+
+    # get a one-hot-encode-esque matrix of genres, then join on them
+    movie_genres = pd.read_csv(hetrec + 'movie_genres.dat', sep='\t').pivot_table(index=['movieID'], columns=['genre'], aggfunc=[len], fill_value=0)
+    movie_genres.columns = movie_genres.columns.droplevel(0)
+    movie_genres = movie_genres.reset_index()
+    user_item_matrix = pd.merge(user_item_matrix, movie_genres, on='movieID')
+
+    # get a one-hot-encode matrix of countries, then join on them
+    movie_countries = pd.get_dummies(pd.read_csv(hetrec + 'movie_countries.dat', sep='\t'))
+    user_item_matrix = pd.merge(user_item_matrix, movie_countries, on='movieID').sort_values(by=['userId', 'movieID'])
+
+    user_item_matrix.to_csv('user_item_matrix.tsv', sep='\t', index=False)
+
+    give_test = lambda obj: obj.loc[np.random.choice(obj.index, len(obj.index) // 10), :]
+    test_data = user_item_matrix.groupby('userId', as_index=False).apply(give_test).reset_index(level=0, drop=True)
+    train_data = user_item_matrix[~user_item_matrix.index.isin(test_data.index)]
+
+    # user item stats
+    all_data = user_item_matrix
+    num_user = len(all_data['userId'].unique()) + 1
+    num_item = len(all_data['movieID'].unique()) + 1
+
+    # convert input to torch tensors
+    train_tensors = [torch.tensor(columnData.values, device=device) for _, columnData in train_data.iteritems()]
+    test_tensors = [torch.tensor(columnData.values, device=device) for _, columnData in test_data.iteritems()]
+
+    # convert tensors to dataloader
+    train_dataset = data.TensorDataset(*train_tensors)
+    train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataset = data.TensorDataset(*test_tensors)
+    test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    print('train_samples:{} \t test_samples:{} \t num_user:{} \t num_item:{}'.format(train_data.shape[0], test_data.shape[0], num_user, num_item))
+    return train_loader, test_loader, num_user, num_item
 
 
-def train(lr, batch_size, output_dim=32, practice=True):
-	train_loader, test_loader, user_num, item_num = process_data(device=device, batch_size=batch_size)
+def train(lr, batch_size, output_dim=32):
+    train_loader, test_loader, user_num, item_num = process_data(device=device, batch_size=batch_size)
 
-	if practice:
-		model = simpleCF(user_num, item_num, output_dim)
-	else:
-		model = NCF(user_num, item_num, output_dim)
+    model = simpleCF(user_num, item_num, output_dim)
 
-	model = model.to(device)
-	loss_function = nn.MSELoss()
-	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    model = model.to(device)
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-	for epoch in range(1, 20):
-		# training loop
-		start_time = time.time()
-		model.train()
-		for user, item, label in tqdm(train_loader, total=len(train_loader)):
-			user = user.to(device)
-			item = item.to(device)
-			label = label.float().to(device)
-			model.zero_grad()
-			prediction = model(user, item)
-			loss = loss_function(prediction, label)
-			loss.backward()
-			optimizer.step()
+    for epoch in range(1, 20):
+        # training loop
+        model.train()
+        # for user, item, label in tqdm(train_loader, total=len(train_loader)):
+        for features in tqdm(train_loader, total=len(train_loader)):
+            label = features[2].float().to(device)
+            model.zero_grad()
+            prediction = model([feature.to(device) for feature in features[:2] + features[3:]])
+            loss = loss_function(prediction, label)
+            loss.backward()
+            optimizer.step()
 
-		# eval loop
-		test_mae = []
-		model.eval()
-		for user, item, label in test_loader:
-			user = user.to(device)
-			item = item.to(device)
-			prediction = model(user, item)
-			label = label.float().detach().cpu().numpy()
-			prediction = prediction.float().detach().cpu().numpy()
-			MAE = mean_absolute_error(y_pred=prediction, y_true=label)
-			test_mae.append(MAE)
+        # eval loop
+        test_mae = []
+        model.eval()
+        # for user, item, label in test_loader:
+        for features in test_loader:
+            prediction = model([feature.to(device) for feature in features[:2] + features[3:]])
+            label = features[2].float().detach().cpu().numpy()
+            prediction = prediction.float().detach().cpu().numpy()
+            test_mae.append(mean_absolute_error(y_pred=prediction, y_true=label))
 
-		print ('Epoch:{} -> test MAE:{}'.format(epoch, np.mean(test_mae)))
-		continue
+        print('Epoch:{} -> test MAE:{}'.format(epoch, np.mean(test_mae)))
 
-# run this to test your simpleCF
-train(lr=0.0005, batch_size=256, output_dim=32, practice=True)
 
-# run this to compare with full neuralCF
-train(lr=0.0005, batch_size=256, output_dim=32, practice=False)
-
+train(lr=0.0005, batch_size=256, output_dim=32)
