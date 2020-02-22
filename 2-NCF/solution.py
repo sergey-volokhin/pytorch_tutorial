f7e761ff9f9ee56ff602456e9ff1adf6c84df606
Sergey <>
2020-02-22 01:38:25 -0500

    fix

--- 2-NCF/solution.py
+++ 2-NCF/solution.py
@@ -11,7 +11,7 @@ class simpleCF(nn.Module):
         self.country_emb = nn.Embedding(country_num, output_dim * 4)
         self.tags_emb = nn.Embedding(tags_num, output_dim * 4)
 
-        self.linear_1 = nn.Linear(output_dim * 8, output_dim * 4)
+        self.linear_1 = nn.Linear(output_dim * 20, output_dim * 4)
         self.linear_2 = nn.Linear(output_dim * 4, output_dim * 2)
         self.linear_3 = nn.Linear(output_dim * 2, 1)
         self.dropout = nn.Dropout()
@@ -27,7 +27,7 @@ class simpleCF(nn.Module):
         for i in [user_emb, item_emb, genre_emb, country_emb, tags_emb]:
             print(i.shape)
         concat = torch.cat((user_emb, item_emb, genre_emb, country_emb, tags_emb), -1)
-
+        print('concat.shape', concat.shape)
         x = self.linear_1(concat)
         x = self.relu(x)
         x = self.dropout(x)
